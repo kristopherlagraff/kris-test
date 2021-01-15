cd912223235f0b9bfd2abcb810cb80fed05c020f
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 13:57:27 -0700

    sadkfhjdsjf

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -4,7 +4,7 @@ echo "Replacing previous environment urls with new environment urls... \n";
 if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
     case 'live':
-      passthru('wp search-replace "http://test-kris-test.pantheonsite.io" "http://changed.pantheonsite.io" --all-tables ');
+      passthru('wp search-replace "https://dev-kris-test.pantheonsite.io" "http://test-kris-test.pantheonsite.io" --all-tables ');
       break;
     case 'test':
       passthru('wp search-replace "http://test-kris-test.pantheonsite.io" ":http://changed.pantheonsite.io" --all-tables ');
