6ae70d6b16b17b7f2155c69a4e2aead7859ca10b
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 13:47:21 -0700

    adfsds

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -3,8 +3,11 @@ echo "Replacing previous environment urls with new environment urls... \n";
 
 if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
-    case 'test':
+    case 'live':
       passthru('wp search-replace "http://test-kris-test.pantheonsite.io" "http://changed.pantheonsite.io" --all-tables ');
+      break;
+    case 'test':
+      passthru('wp search-replace "http://test-kris-test.pantheonsite.io" ":http://changed.pantheonsite.io" --all-tables ');
       break;
   }
 }
