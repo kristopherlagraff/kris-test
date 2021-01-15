8b383e8ba030054939ad3c812b0f23be61611981
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 15:03:47 -0700

    QS SR 2

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -4,7 +4,7 @@ echo "Replacing previous environment urls with new environment urls... \n";
 if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
     case 'test':
-      passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://changed.pantheonsite.io" --all-tables ');
+      passthru('wp search-replace "://dev-kris-test.pantheonsite.io/" "://changed.pantheonsite.io" --all-tables ');
       break;
   }
 }
