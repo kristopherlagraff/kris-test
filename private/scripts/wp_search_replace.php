1c37060fc32977a0c8e9fc3484559badc8cca1f8
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 12:34:37 -0700

    another QS test

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -7,9 +7,7 @@ if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
       passthru('wp search-replace "://dev-kris-test.pantheonsite.io" "://example.com" --all-tables ');
       break;
     case 'test':
-      passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://test-examplesite.pantheonsite.io" --all-tables ');
-      passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://test-examplesite.pantheonsite.io" --all-tables ');
-      passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://test-examplesite.pantheonsite.io" --all-tables ');
+      passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://changed.pantheonsite.io" --all-tables ');
       break;
   }
 }
