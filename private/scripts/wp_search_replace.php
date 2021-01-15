aaef26bc3dbef2ffc01df3b0626443e431a2288b
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 12:45:01 -0700

    try again

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -3,9 +3,6 @@ echo "Replacing previous environment urls with new environment urls... \n";
 
 if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
-    case 'live':
-      passthru('wp search-replace "://dev-kris-test.pantheonsite.io" "://example.com" --all-tables ');
-      break;
     case 'test':
       passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://changed.pantheonsite.io" --all-tables ');
       break;
