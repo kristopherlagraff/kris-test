f1211663484c939f0f263e771298774293a6bbbe
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 13:34:34 -0700

    dosfuhadsfhd

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -4,9 +4,8 @@ echo "Replacing previous environment urls with new environment urls... \n";
 if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
     case 'test':
-      passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://changed.pantheonsite.io" --all-tables ');
+      passthru('wp search-replace "http://test-kris-test.pantheonsite.io" "http://changed.pantheonsite.io" --all-tables ');
       break;
   }
 }
 ?>
-
