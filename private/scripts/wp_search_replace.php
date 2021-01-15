b6bc3fa1d2bf4a6932e46d7ec9d5f9a1d1cf2c0f
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-15 13:31:39 -0700

    yml changed

--- private/scripts/wp_search_replace.php
+++ private/scripts/wp_search_replace.php
@@ -3,13 +3,10 @@ echo "Replacing previous environment urls with new environment urls... \n";
 
 if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
   switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
-    default:
-      putenv('WP_ENVIRONMENT_TYPE=development');
-      passthru('wp search-replace "://dev-kris-test.pantheonsite.io" "://test-kris-test.pantheonsite.io" --all-tables ');
-      break;
     case 'test':
       passthru('wp search-replace "://test-kris-test.pantheonsite.io" "://changed.pantheonsite.io" --all-tables ');
       break;
   }
 }
 ?>
+
