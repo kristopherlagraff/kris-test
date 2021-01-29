68bc7f69212a888898ec3e3d22b784a6277c10fe
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-01-29 16:08:11 -0700

    testing config error

--- wp-config.php
+++ wp-config.php
@@ -12,7 +12,7 @@
  * Pantheon platform settings. Everything you need should already be set.
  */
 if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-	require_once(dirname(__FILE__) . '/p-config-pantheon.php');
+	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
 
 /**
  * Local configuration information.
