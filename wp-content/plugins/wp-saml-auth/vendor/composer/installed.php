579675e22955637c0fe7a1b601e3348b93454bf0
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-02-22 12:04:37 -0700

    testing logout

---
+++ wp-content/plugins/wp-saml-auth/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ce961beb92865755fb8f56c5063fec0876dcad75',
+    'name' => 'pantheon-systems/wp-saml-auth',
+  ),
+  'versions' => 
+  array (
+    'onelogin/php-saml' => 
+    array (
+      'pretty_version' => '3.6.0',
+      'version' => '3.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '888e33356d0d7b529991a431fce3ee20c0307988',
+    ),
+    'pantheon-systems/wp-saml-auth' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ce961beb92865755fb8f56c5063fec0876dcad75',
+    ),
+    'robrichards/xmlseclibs' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f8f19e58f26cdb42c54b214ff8a820760292f8df',
+    ),
+  ),
+);
