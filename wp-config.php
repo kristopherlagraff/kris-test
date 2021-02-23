b8eddcc45dfe10eb456a3280bc5a0e943f1590d9
Kristopher LaGraff <kristopherlagraff@kristophers-mbp.lan>
2021-02-23 09:59:12 -0700

    testing php auth

--- wp-config.php
+++ wp-config.php
@@ -72,7 +72,15 @@ $table_prefix = 'wp_';
  * You may want to examine $_ENV['PANTHEON_ENVIRONMENT'] to set this to be
  * "true" in dev, but false in test and live.
  */
-
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
+    exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+}
 
 
 if ( ! defined( 'WP_DEBUG' ) ) {
