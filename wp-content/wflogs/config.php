b94c72f36e8fe59d763aff29ce9da4599d59ac35
Kristopher LaGraff <kristopher.lagraff@getpantheon.com>
2021-03-03 23:47:11 +0000

    wflogs

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1615244855;s:7:"authKey";s:64:"$68YBF+)J]Y A[j0L=;(2e`PZbL-7kRJ|JNHRhR_K#&;85|cD~UNtN?HSkd^+vfD";s:7:"version";s:5:"1.0.4";}
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1615244855;s:7:"authKey";s:64:"$68YBF+)J]Y A[j0L=;(2e`PZbL-7kRJ|JNHRhR_K#&;85|cD~UNtN?HSkd^+vfD";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;}
